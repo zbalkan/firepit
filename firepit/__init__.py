58d6541057c4275408e5d807127eea4beeabd295
Zafer Balkan <zafer@zaferbalkan.com>
2026-09-09 23:58:38 +0300

    cleanup: remove obsolete project scaffolding and legacy fixtures

--- firepit/__init__.py
+++ firepit/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = "IBM Security"
 __email__ = "pcoccoli@us.ibm.com"
-__version__ = "2.3.35"
+__version__ = "3.0.0"
 
 from firepit.storage import get_storage as _get_storage
 from firepit.storageurl import storage_path
--- firepit/exceptions.py
+++ firepit/exceptions.py
@@ -1,14 +1,6 @@
-class InvalidAttr(Exception):
-    """A requested DuckDB/STIX attribute is invalid."""
-
-
 class InvalidObject(Exception):
     """Input is not valid for the STIX 2.1 storage boundary."""
 
 
 class InvalidViewname(Exception):
     """A SQL identifier supplied as a table/view/session name is invalid."""
-
-
-class UnknownViewname(Exception):
-    """A requested DuckDB table or view does not exist."""
