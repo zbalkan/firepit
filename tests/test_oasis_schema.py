ad050269c723e1a52c2307766b27af261c10a3b7
Zafer Balkan <zafer@zaferbalkan.com>
2026-09-10 09:20:34 +0300

    tests: remove test-only STIX registry surface

--- tests/test_oasis_schema.py
+++ tests/test_oasis_schema.py
@@ -1,16 +1,7 @@
 import pytest
 
-from firepit._stix import standard_object_types, validate_object
+from firepit._stix import validate_object
 from firepit.exceptions import InvalidObject
-
-
-def test_oasis_registry_is_authoritative_for_standard_types():
-    types = standard_object_types()
-    assert "indicator" in types
-    assert "threat-actor" in types
-    assert "relationship" in types
-    assert "process" in types
-    assert "network-traffic" in types
 
 
 def test_oasis_indicator_schema_is_enforced():
